a5d40a2e11198988a37bd04384e4a22f66f1b883
manyids2 <arun.mukundan@gmail.com>
2023-12-15 08:50:54 +0100

    All functions from paper copied, now need to understand, write tests

--- src/tidy-tree.c
+++ src/tidy-tree.c
@@ -126,7 +126,7 @@ void tree_free(Tree *t) {
 
 void tree_layout(Tree *t) {
   tree_first_walk(t);
-  // tree_second_walk(t, 0);
+  tree_second_walk(t, 0);
 }
 
 void tree_first_walk(Tree *t) {
@@ -136,17 +136,17 @@ void tree_first_walk(Tree *t) {
   }
   tree_first_walk(t->c[0]);
 
-  // IYL *ih = malloc(sizeof(IYL));
-  // IYL_update(tree_bottom(t->c[0]->el), 0, NULL, ih);
+  IYL *ih = malloc(sizeof(IYL));
+  iyl_update(tree_bottom(t->c[0]->el), 0, NULL, ih);
 
-  // for (int i = 1; i < t->cs; i++) {
-  //   tree_first_walk(t->c[i]);
-  //   double minY = tree_bottom(t->c[i]->er);
-  //   tree_separate(t, i, ih);
-  //   IYL_update(minY, i, ih, ih);
-  // }
-  //
-  // tree_position_root(t);
+  for (int i = 1; i < t->cs; i++) {
+    tree_first_walk(t->c[i]);
+    double minY = tree_bottom(t->c[i]->er);
+    tree_separate(t, i, ih);
+    iyl_update(minY, i, ih, ih);
+  }
+
+  tree_position_root(t);
   tree_set_extremes(t);
 }
 
@@ -163,15 +163,130 @@ void tree_set_extremes(Tree *t) {
   }
 }
 
-// void tree_second_walk(Tree *t, double modsum) {
-//   modsum += t->mod;
-//   // Set absolute (non-relative) horizontal coordinate
-//   t->x = t->prelim + modsum;
-//   tree_add_child_spacing(t);
-//   for (int i = 0; i < t->cs; i++) {
-//     tree_second_walk(t->c[i], modsum);
-//   }
-// }
+void tree_separate(Tree *t, int i, IYL *ih) {
+  // Right contour node of left siblings and its sum of modifiers.
+  Tree *sr = t->c[i - 1];
+  double mssr = sr->mod;
+
+  // Left contour node of current subtree and its sum of modifiers.
+  Tree *cl = t->c[i];
+  double mscl = cl->mod;
+
+  while ((sr != NULL) && (cl != NULL)) {
+    if (tree_bottom(sr) > ih->lowY) {
+      ih = ih->nxt;
+    }
+
+    // How far to the left of the right side of sr is the left side of cl?
+    double dist = (mssr + sr->prelim + sr->w) - (mscl + cl->prelim);
+    if (dist > 0) {
+      mscl += dist;
+      tree_move_subtree(t, i, ih->index, dist);
+    }
+    double sy = tree_bottom(sr);
+    double cy = tree_bottom(cl);
+
+    // Advance highest node(s) and sum(s) of modifiers
+    // ( Coordinate system increases downwards )
+    if (sy <= cy) {
+      sr = tree_next_right_contour(sr);
+      if (sr != NULL) {
+        mssr += sr->mod;
+      }
+    }
+    if (sy >= cy) {
+      cl = tree_next_left_contour(cl);
+      if (cl != NULL) {
+        mscl += cl->mod;
+      }
+    }
+  }
+
+  // Set threads and update extreme nodes.
+  if ((sr == NULL) && (cl != NULL)) {
+    // Current subtree must be taller than left siblings.
+    tree_set_left_thread(t, i, cl, mscl);
+  } else if (sr != NULL && cl == NULL) {
+    // Left siblings must be taller than current subtree.
+    tree_set_right_thread(t, i, sr, mssr);
+  }
+}
+
+void tree_move_subtree(Tree *t, int i, int si, f64 dist) {
+  // Move subtree by changing mod.
+  t->c[i]->mod += dist;
+  t->c[i]->msel += dist;
+  t->c[i]->mser += dist;
+  tree_distribute_extra(t, i, si, dist);
+}
+
+Tree *tree_next_left_contour(Tree *t) { return t->cs == 0 ? t->tl : t->c[0]; }
+Tree *tree_next_right_contour(Tree *t) {
+  return t->cs == 0 ? t->tr : t->c[t->cs - 1];
+}
+double tree_bottom(Tree *t) { return t->y + t->h; }
+
+void tree_set_left_thread(Tree *t, int i, Tree *cl, f64 modsumcl) {
+  Tree *li = t->c[0]->el;
+  li->tl = cl;
+  // Change mod so that sum of modifier after following thread is correct
+  double diff = (modsumcl - cl->mod) - t->c[0]->msel;
+  li->mod += diff;
+  // Change preliminary x coord so that the node does not move
+  li->prelim -= diff;
+  // Update extreme node and its sum of modifiers.
+  t->c[0]->el = t->c[i]->el;
+  t->c[0]->msel = t->c[i]->msel;
+}
+
+void tree_set_right_thread(Tree *t, int i, Tree *sr, f64 modsumsr) {
+  Tree *ri = t->c[i]->er;
+  ri->tr = sr;
+  // Change mod so that sum of modifier after following thread is correct
+  double diff = (modsumsr - sr->mod) - t->c[i]->mser;
+  ri->mod += diff;
+  // Change preliminary x coord so that the node does not move
+  ri->prelim -= diff;
+  // Update extreme node and its sum of modifiers.
+  t->c[i]->er = t->c[i - 1]->er;
+  t->c[i]->mser = t->c[i - 1]->mser;
+}
+
+void tree_position_root(Tree *t) {
+  t->prelim = (t->c[0]->prelim + t->c[0]->mod + t->c[t->cs - 1]->mod +
+               t->c[t->cs - 1]->prelim + t->c[t->cs - 1]->w) /
+                  2 -
+              t->w / 2;
+}
+
+void tree_distribute_extra(Tree *t, int i, int si, f64 dist) {
+  if (si != i - 1) {
+    double nr = i - si;
+    t->c[si + 1]->shift += dist / nr;
+    t->c[i]->shift -= dist / nr;
+    t->c[i]->change -= dist - dist / nr;
+  }
+}
+
+void tree_second_walk(Tree *t, double modsum) {
+  modsum += t->mod;
+  // Set absolute (non-relative) horizontal coordinate
+  t->x = t->prelim + modsum;
+  tree_add_child_spacing(t);
+  for (int i = 0; i < t->cs; i++) {
+    tree_second_walk(t->c[i], modsum);
+  }
+}
+
+void tree_add_child_spacing(Tree *t) {
+  double d = 0;
+  double modsumdelta = 0;
+  for (int i = 0; i < t->cs; i++) {
+    d += t->c[i]->shift;
+    modsumdelta += d + t->c[i]->change;
+    t->c[i]->mod += modsumdelta;
+  }
+}
 
 long read_size_from_file(char *path) {
   FILE *fp;
@@ -230,7 +345,7 @@ void arena_parse_content(Arena *a) {
   }
   a->max_indent = max_indent;
 
-  // Reset arena size to free excess
+  // Reset arena capacity to number of nodes
   arena_shrink_to_len(a);
 
   // Put into tree structure
